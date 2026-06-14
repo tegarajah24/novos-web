d98281be16417928c4f0d0923b67dbd9d95646a1
Andrea Dev <fadliandrea06@gmail.com>
2026-06-14 21:34:23 +0700

    menghapus keterangan hari dan merubah warna teks daftar pesanan

--- resources/views/internal/daftar-pesanan.blade.php
+++ resources/views/internal/daftar-pesanan.blade.php
@@ -3,8 +3,7 @@
 @section('title', 'Daftar Pesanan')
 
 @section('topbar-left')
-    <h1 class="text-xl font-bold text-[#1a237e]">Daftar Pesanan</h1>
-    <p class="text-sm text-gray-500 mt-0.5">{{ \Carbon\Carbon::now()->locale('id')->isoFormat('dddd, D MMMM YYYY') }}</p>
+    <h1 class="text-xl font-bold text-black">Daftar Pesanan</h1>
 @endsection
 
 @section('internal-content')
