e9fa01d660ae267c454679ec50df35eee2b89226
tegarajah24 <tegarrifai1410@gmail.com>
2026-06-21 15:28:04 +0700

    membuat shortcut pada card summary

--- app/Http/Controllers/Internal/DashboardController.php
+++ app/Http/Controllers/Internal/DashboardController.php
@@ -106,6 +106,7 @@ class DashboardController extends Controller
                 'up' => $totalOrders >= $lastMonthOrders,
                 'bg' => 'bg-blue-50', 'tc' => 'text-blue-600',
                 'icon' => 'M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z',
+                'url' => route('staf.daftar-pesanan'),
             ],
             [
                 'v' => $totalRevenue > 0 ? 'Rp ' . number_format($totalRevenue / 1000000, 1) . 'jt' : 'Rp 0',
@@ -114,6 +115,7 @@ class DashboardController extends Controller
                 'up' => $totalRevenue >= $lastMonthRevenue,
                 'bg' => 'bg-green-50', 'tc' => 'text-green-600',
                 'icon' => 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+                'url' => route('staf.laporan', ['filter' => 'month']),
             ],
             [
                 'v' => (string) $activeCustomers,
@@ -122,6 +124,7 @@ class DashboardController extends Controller
                 'up' => true,
                 'bg' => 'bg-indigo-50', 'tc' => 'text-indigo-600',
                 'icon' => 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
+                'url' => route('staf.kelola-pengguna'),
             ],
             [
                 'v' => $avgDaysFormatted,
@@ -130,6 +133,7 @@ class DashboardController extends Controller
                 'up' => false,
                 'bg' => 'bg-orange-50', 'tc' => 'text-orange-500',
                 'icon' => 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z',
+                'url' => route('staf.laporan'),
             ],
         ];
 
@@ -147,6 +151,7 @@ class DashboardController extends Controller
                 'up' => $pendingCount > 0,
                 'bg' => 'bg-yellow-50', 'tc' => 'text-yellow-600',
                 'icon' => 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z',
+                'url' => route('staf.daftar-pesanan', ['status' => 'menunggu_verifikasi']),
             ],
             [
                 'v' => (string) $designCount,
@@ -155,6 +160,7 @@ class DashboardController extends Controller
                 'up' => true,
                 'bg' => 'bg-purple-50', 'tc' => 'text-purple-600',
                 'icon' => 'M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z',
+                'url' => route('staf.daftar-pesanan', ['status' => 'di_design']),
             ],
             [
                 'v' => (string) $completedMonth,
@@ -163,6 +169,7 @@ class DashboardController extends Controller
                 'up' => true,
                 'bg' => 'bg-green-50', 'tc' => 'text-green-600',
                 'icon' => 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+                'url' => route('staf.daftar-pesanan', ['status' => 'selesai', 'date_from' => now()->startOfMonth()->format('Y-m-d'), 'date_to' => now()->endOfMonth()->format('Y-m-d')]),
             ],
             [
                 'v' => (string) $totalSold,
@@ -171,6 +178,7 @@ class DashboardController extends Controller
                 'up' => true,
                 'bg' => 'bg-teal-50', 'tc' => 'text-teal-600',
                 'icon' => 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
+                'url' => route('staf.laporan'),
             ],
         ];
 
