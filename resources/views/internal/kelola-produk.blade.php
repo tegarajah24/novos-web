d49eb7ab45a958edff51d69d05ac16811b97d31b
Andrea Dev <fadliandrea06@gmail.com>
2026-06-27 11:30:51 +0700

    mengubah text heading menu di menu kelola produk

--- resources/views/internal/kelola-produk.blade.php
+++ resources/views/internal/kelola-produk.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Kelola Produk')
 
 @section('topbar-left')
-    <h1 class="text-xl font-bold text-[#1a237e]">Katalog Produk</h1>
+    <h1 class="text-xl font-bold text-[#1a237e]">Kelola Produk</h1>
 @endsection
 
 @section('internal-content')
