734ec84bb574ee7a00228093ddb5fbe5100e33c8
tegarajah24 <tegarrifai1410@gmail.com>
2026-07-02 22:26:58 +0700

    fix: hapus import alpinejs ganda (app.js + Livewire inject)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,7 @@
-import Alpine from 'alpinejs'
 import Swal from 'sweetalert2'
 import { createIcons, icons } from 'lucide'
 
-window.Alpine = Alpine
 window.Swal = Swal
 window.lucide = { createIcons, icons }
-
-Alpine.start()
 
 createIcons({ icons })
