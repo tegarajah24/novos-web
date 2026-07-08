47604ece8e9ff834aaea9da0eab0f7f8ef28d877
tegarajah24 <tegarrifai1410@gmail.com>
2026-07-08 13:17:04 +0700

    posisikan seluruh gambar referensi desain bersandingan di dalam satu kotak frame yang sama pada ekspor excel

--- resources/views/internal/kelola-pengguna.blade.php
+++ resources/views/internal/kelola-pengguna.blade.php
@@ -55,7 +55,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     <input type="text" id="searchInput" placeholder="Cari pengguna..." class="w-full pl-10 pr-4 py-2 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#1a237e]/20 focus:border-[#1a237e] transition-all">
                 </div>
-                <select id="roleFilter" class="px-4 py-2 border border-gray-200 rounded-lg text-sm text-gray-600 bg-white focus:outline-none focus:ring-2 focus:ring-[#1a237e]/20 focus:border-[#1a237e] transition-all">
+                <select id="roleFilter" class="w-44 px-4 py-2 border border-gray-200 rounded-lg text-sm text-gray-600 bg-white focus:outline-none focus:ring-2 focus:ring-[#1a237e]/20 focus:border-[#1a237e] transition-all">
                     <option value="">Semua Role</option>
                     <option value="Super Admin">Super Admin</option>
                     <option value="Manager">Manager</option>
