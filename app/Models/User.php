13b411d4973f7546a34270c7140199856196ead8
tegarajah24 <tegarrifai1410@gmail.com>
2026-07-18 13:27:29 +0700

    fix: add public_title to Fillable attribute in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'phone', 'address', 'role_id', 'avatar', 'fullname'])]
+#[Fillable(['name', 'email', 'password', 'phone', 'address', 'role_id', 'avatar', 'fullname', 'public_title'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
